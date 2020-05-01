73f3d62914e8b0a3bfd38386bffe8a834f613c9b
Nitesh Srivats <niteshsrivats.k@gmail.com>
2020-05-01 15:27:39 +0530

    Changed port number to 8000

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
+var express = require("express");
+var http = require("http");
+var path = require("path");
 var logger = require("morgan");
-var events = require('./events');
-var gallery = require('./gallery');
+var events = require("./events");
+var gallery = require("./gallery");
 var app = express();
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 8000;
 
-process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = "production";
 
 app.use(logger("dev"));
 
@@ -16,38 +16,36 @@ var publicPath = path.resolve(__dirname, "public");
 
 app.use(express.static(publicPath));
 
-app.use(function(request, response, next) {
-    console.log("In comes a " + request.method + " to " + request.url);
-    next();
+app.use(function (request, response, next) {
+  console.log("In comes a " + request.method + " to " + request.url);
+  next();
 });
 
-app.get('/', function(req, res) {
-    app.locals.json = events;
-    app.locals.gallery = gallery;
-    res.render("index");
+app.get("/", function (req, res) {
+  app.locals.json = events;
+  app.locals.gallery = gallery;
+  res.render("index");
 });
 
-app.get('/ticket', function(req, res) {
-    app.locals.json = events;
-    res.render("ticket");
+app.get("/ticket", function (req, res) {
+  app.locals.json = events;
+  res.render("ticket");
 });
 
-app.get('/event-schedule', function(req, res) {
-    res.render("event-schedule");
+app.get("/event-schedule", function (req, res) {
+  res.render("event-schedule");
 });
 
-app.get('/team', function(req, res) {
-    res.render("team");
+app.get("/team", function (req, res) {
+  res.render("team");
 });
 
-app.get('/*', function(req, res) {
-    res.render("404");
+app.get("/*", function (req, res) {
+  res.render("404");
 });
 
 app.set("view engine", "ejs");
 
-app.listen(PORT, function() {
-    console.log("Started at " + PORT);
-})
-
-
+app.listen(PORT, function () {
+  console.log("Started at " + PORT);
+});
